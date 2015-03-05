eb97e7852d1dbec20c8272e12c6b16de27e0a8b9
kingosticks <kingosticks@gmail.com>
2015-03-05 22:19:16 +0000

    Fixed wrong license specified

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='Mopidy-MusicBox-Webclient',
     version=get_version('mopidy_musicbox_webclient/__init__.py'),
     url='https://github.com/woutervanwijk/mopidy-musicbox-webclient',
-    license='GNU General Public License v3 (GPLv3)',
+    license='Apache License, Version 2.0',
     author='Wouter van Wijk',
     author_email='woutervanwijk@gmail.com',
     description='Mopidy MusicBox web extension',
