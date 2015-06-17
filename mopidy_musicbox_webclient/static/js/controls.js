5e1dc94472319ae178bfae55b868582b5dc6ee39
Davis Mosenkovs <davikovs@gmail.com>
2015-06-17 07:51:32 +0300

    Do nothing if Queue is empty

--- mopidy_musicbox_webclient/static/js/controls.js
+++ mopidy_musicbox_webclient/static/js/controls.js
@@ -228,25 +228,27 @@ function clearQueue() {
 }
 
 function saveQueue() {
-    var plname = window.prompt("Playlist name:", "");
-    if (plname != null && plname.trim() != "") {
-        plname = plname.trim();
-        mopidy.playlists.filter({"name": plname}).then(function(existing) {
-            var exists = false;
-            for (var i = 0; i < existing.length; i++) {
-                exists = exists || existing[i].uri.indexOf("m3u:") == 0 || existing[i].uri.indexOf("local:") == 0;
-            }
-            if (!exists || window.confirm("Overwrite existing playlist \"" + plname + "\"?")) {
-                mopidy.tracklist.getTracks().then(function(tracks) {
-                    mopidy.playlists.create(plname, "local").then(function(playlist) {
-                        playlist.tracks = tracks;
-                        mopidy.playlists.save(playlist).then();
-                        getPlaylists();
-                    });
+    mopidy.tracklist.getTracks().then(function(tracks) {
+        if (tracks.length > 0) {
+            var plname = window.prompt("Playlist name:", "");
+            if (plname != null && plname.trim() != "") {
+                plname = plname.trim();
+                mopidy.playlists.filter({"name": plname}).then(function(existing) {
+                    var exists = false;
+                    for (var i = 0; i < existing.length; i++) {
+                        exists = exists || existing[i].uri.indexOf("m3u:") == 0 || existing[i].uri.indexOf("local:") == 0;
+                    }
+                    if (!exists || window.confirm("Overwrite existing playlist \"" + plname + "\"?")) {
+                        mopidy.playlists.create(plname, "local").then(function(playlist) {
+                             playlist.tracks = tracks;
+                             mopidy.playlists.save(playlist).then();
+                             getPlaylists();
+                        });
+                    }
                 });
             }
-        });
-    }
+        }
+    });
     return false;
 }
 
