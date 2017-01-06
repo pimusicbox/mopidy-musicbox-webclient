12412cc3ac6fbf9567e2660dfb2a0468741a9f46
fbarresi <fede.barresi@gmail.com>
2017-01-06 22:39:56 +0100

    restored dictionary on handler init

--- mopidy_musicbox_webclient/web.py
+++ mopidy_musicbox_webclient/web.py
@@ -74,6 +74,25 @@ class UploadHandler(tornado.web.RequestHandler):
 
         webclient = mmw.Webclient(config)
 
+        if webclient.is_music_box():
+            program_name = 'MusicBox'
+        else:
+            program_name = 'Mopidy'
+
+        url = urlparse.urlparse('%s://%s' % (self.request.protocol, self.request.host))
+        port = url.port or 80
+
+        self.__dict = {
+            'isMusicBox': json.dumps(webclient.is_music_box()),
+            'websocketUrl': webclient.get_websocket_url(self.request),
+            'hasAlarmClock': json.dumps(webclient.has_alarm_clock()),
+            'onTrackClick': webclient.get_default_click_action(),
+            'programName': program_name,
+            'hostname': url.hostname,
+            'serverIP': socket.gethostbyname(url.hostname),
+            'serverPort': port
+
+        }
         self.__path = path
         self.__title = string.Template('{} on $hostname'.format(program_name))
         self.__upload_path = webclient.get_upload_path()
