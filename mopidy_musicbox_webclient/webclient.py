pimusicbox/mopidy-musicbox-webclient

commit 411a3781fdab270984299e47f136abec8395ff57

Combine .js and .py coverage reports.
Remove services that cannot be searched from search dropdown.

Refactoring and additional unit tests.

mopidy_musicbox_webclient/webclient.py
@@ -1,0 +1,46 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+
+from mopidy_musicbox_webclient import Extension
+
+logger = logging.getLogger(__name__)
+
+
+class Webclient(object):
+
+    def __init__(self, config):
+        self.config = config
+
+    @property
+    def ext_config(self):
+        return self.config.get(Extension.ext_name, {})
+
+    @classmethod
+    def get_version(cls):
+        return Extension.version
+
+    def get_websocket_url(self, request):
+        host, port = self.ext_config['websocket_host'], self.ext_config['websocket_port']
+        ws_url = ''
+        if host or port:
+            if not host:
+                host = request.host.partition(':')[0]
+                logger.warning('Musicbox websocket_host not specified, '
+                               'using %s', host)
+            elif not port:
+                port = self.config['http']['port']
+                logger.warning('Musicbox websocket_port not specified, '
+                               'using %s', port)
+            protocol = 'ws'
+            if request.protocol == 'https':
+                protocol = 'wss'
+            ws_url = "%s://%s:%d/mopidy/ws" % (protocol, host, port)
+
+        return ws_url
+
+    def has_alarm_clock(self):
+        return self.ext_config.get('alarmclock', {}).get('enabled', False)
+
+    def is_music_box(self):
+        return self.ext_config.get('musicbox', False)

.gitignore
@@ -8,7 +8,7 @@
 MANIFEST
 build/
 cover/
-coverage/
+.karma_coverage/
 coverage.xml
 dist/
 docs/_build/

.travis.yml
@@ -1,9 +1,17 @@
-sudo: false
+sudo: required
+dist: trusty
 
 language: python
 
 python:
   - "2.7_with_system_site_packages"
+
+addons:
+  apt:
+    sources:
+      - mopidy-stable
+    packages:
+      - mopidy
 
 env:
   - TOX_ENV=py27
@@ -13,6 +21,11 @@ env:
   - TOX_ENV=csslint
   - TOX_ENV=tidy
 
+before_install:
+  - "sudo sed -i '/127.0.1.1/d' /etc/hosts"  # Workaround https://github.com/tornadoweb/tornado/issues/1573
+  - "sudo apt-get update -qq"
+  - "sudo apt-get install -y gstreamer0.10-plugins-good python-gst0.10"
+
 install:
   - "pip install tox"
 
@@ -20,6 +33,4 @@ script:
   - "tox -e $TOX_ENV"
 
 after_success:
-  # TODO: find a way to combine .py and .js coverage reports.
-  # - "if [ $TOX_ENV == 'py27' ]; then pip install coveralls; coveralls; fi"
-  - "if [ $TOX_ENV == 'test' ]; then cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js; fi"
+  - "if [ $TOX_ENV == 'test' ]; then pip install coveralls; coveralls --merge=./karma_coverage/coverage-final.json; fi"

karma.conf.js
@@ -71,9 +71,10 @@ module.exports = function (config) {
 
         coverageReporter: {
             // specify a common output directory
-            dir: 'coverage/',
+            dir: '.karma_coverage/',
             reporters: [
                 { type: 'lcov', subdir: '.' },
+                { type: 'json', subdir: '.' },
                 { type: 'text' }
             ]
         }

mopidy_musicbox_webclient/__init__.py
@@ -7,7 +7,7 @@ from mopidy import config, ext
 __version__ = '2.2.0'
 
 
-class MusicBoxExtension(ext.Extension):
+class Extension(ext.Extension):
 
     dist_name = 'Mopidy-MusicBox-Webclient'
     ext_name = 'musicbox_webclient'
@@ -18,7 +18,7 @@ class MusicBoxExtension(ext.Extension):
         return config.read(conf_file)
 
     def get_config_schema(self):
-        schema = super(MusicBoxExtension, self).get_config_schema()
+        schema = super(Extension, self).get_config_schema()
         schema['musicbox'] = config.Boolean(optional=True)
         schema['websocket_host'] = config.Hostname(optional=True)
         schema['websocket_port'] = config.Port(optional=True)

mopidy_musicbox_webclient/static/index.html
@@ -1,30 +1,14 @@
 <!DOCTYPE html>
 <html manifest="mb.appcache">
 <head>
-    <title>Musicbox</title>
+    <title>{{ title }}</title>
     <meta charset="utf-8">
-    <script type="text/javascript" src="vendors/jquery/jquery-1.12.0.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="vendors/jquery_mobile_flat_ui_theme/jquery.mobile.flatui.min.css"/>
-    <script>
-        //configuration
-        var isMusicBox = '{{musicbox}}' == 'True'; // Remove MusicBox only content (e.g. settings, system pages)
-        var websocketUrl = ('{{useWebsocketUrl}}' == 'True') ? '{{websocket_url}}' : ''
-        var hasAlarmClock = '{{alarmclock}}' == 'True'; // Add Alarm Clock icons
 
-        $(document).bind("mobileinit", function () {
-            $.extend($.mobile, {
-                ajaxEnabled: false,
-                hashListeningEnabled: false
-                //                    linkBindingEnabled: false
-                //                  buttonMarkup.hoverDelay: 100,
-                //                  buttonMarkup.corners: false
-            });
-        });
-/*        window.addEventListener('load', function () {
-            new FastClick(document.body);
-        }, false);
-*/
-    </script>
+    <script type="text/javascript" src="vendors/jquery/jquery-1.12.0.min.js"></script>
+    <script type="text/javascript" src="vendors/jquery_cookie/jquery.cookie.js"></script>
+    <script type="text/javascript" src="js/custom_scripting.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="vendors/jquery_mobile_flat_ui_theme/jquery.mobile.flatui.min.css"/>
     <link rel="icon" type="image/gif" href="images/icons/musicbox32.gif" />
     <link rel="apple-touch-icon" href="images/icons/musicbox57.png" />
     <link rel="apple-touch-icon" sizes="72x72" href="images/icons/musicbox72.png" />
@@ -52,7 +36,7 @@
 
 </head>
 
-<body>
+<body data-websocket-url="{{websocketUrl}}" data-is-musicbox="{{isMusicBox}}" data-has-alarmclock="{{hasAlarmClock}}">
 <div data-role="page" id="page" class="ui-responsive-panel" data-theme="c">
 <div data-role="panel" id="panel" data-position="left" data-theme="a" data-display="reveal" data-position-fixed="true">
 
@@ -501,7 +485,6 @@
 </div>
 <!-- /page one -->
 <script type="text/javascript" src="vendors/mopidy/mopidy.min.js"></script>
-<script type="text/javascript" src="vendors/jquery_cookie/jquery.cookie.js"></script>
 <script type="text/javascript" src="vendors/media_progress_timer/timer.js"></script>
 <script type="text/javascript" src="js/progress_timer.js"></script>
 <script type="text/javascript" src="js/controls.js"></script>

mopidy_musicbox_webclient/static/js/custom_scripting.js
@@ -1,0 +1,8 @@
+// jQuery Mobile configuration options
+// see: http://api.jquerymobile.com/1.3/global-config/
+$(document).bind('mobileinit', function () {
+    $.extend($.mobile, {
+        ajaxEnabled: false,
+        hashListeningEnabled: false
+    })
+})

mopidy_musicbox_webclient/static/js/functionsvars.js
@@ -110,6 +110,19 @@ var uriHumanList = [
     ['subsonic', 'Subsonic']
 ]
 
+// List of Mopidy URI schemes that should not be searched directly.
+// Also blacklists 'yt' in favour of using the other 'youtube' supported scheme.
+var searchBlacklist = [
+    'file',
+    'http',
+    'https',
+    'mms',
+    'rtmp',
+    'rtmps',
+    'rtsp',
+    'yt'
+]
+
 function scrollToTop () {
     var divtop = 0
     $('body,html').animate({

commit d8d040adf30b6f870133c628ec338fc1eb914bea

Fix alarm clock detection
Fix alarm clock detection broken in
411a3781fdab270984299e47f136abec8395ff57

mopidy_musicbox_webclient/webclient.py
@@ -40,7 +40,7 @@ class Webclient(object):
         return ws_url
 
     def has_alarm_clock(self):
-        return self.ext_config.get('alarmclock', {}).get('enabled', False)
+        return self.config.get('alarmclock', {}).get('enabled', False)
 
     def is_music_box(self):
         return self.ext_config.get('musicbox', False)